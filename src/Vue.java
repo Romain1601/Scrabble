cb6ebc8fae05a2518b69de024543ab43dd203464
Lucas Estabes <lucasestabes@Air-de-Lucas.univ-jfc.fr>
2020-03-11 14:31:45 +0100

    V1 affiche du orange

--- src/Vue.java
+++ src/Vue.java
@@ -1,9 +1,23 @@
+import java.awt.Color; 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Vue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JFrame fenetre = new JFrame();            
+		fenetre.setTitle("Ma première fenêtre Java");
+		fenetre.setSize(400, 400);
+		fenetre.setLocationRelativeTo(null);               
 
-	}
+		//Instanciation d'un objet JPanel
+		JPanel pan = new JPanel();
+		//Définition de sa couleur de fond
+		pan.setBackground(Color.ORANGE);        
+		//On prévient notre JFrame que notre JPanel sera son content pane
+		fenetre.setContentPane(pan);               
+		fenetre.setVisible(true);
+	}     
 
-}
+};
