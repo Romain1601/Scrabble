33751bae54f63009c122992a66e3d5876ed13349
loic-dev <loic.charrie.12@gmail.com>
2020-03-11 15:32:17 +0100

    Creation de la classe DictionnairePoint - ref point des lettres

---
+++ src/DictionnairePoint.java
@@ -0,0 +1,17 @@
+import java.util.TreeMap;
+
+public class DictionnairePoint {
+	
+
+
+	public static void main(String[] args) {
+		TreeMap<Char, Integer > liste = new TreeMap<>();
+		liste.put('a', 1);
+		liste.put('b', 3);
+		liste.put('c', 3);
+		liste.put('d', 2);
+		
+
+	}
+
+}
