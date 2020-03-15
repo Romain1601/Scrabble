2bb44bcfa5f08eac0d0c6b1b24f22ae646e3719a
loic-dev <loic.charrie.12@gmail.com>
2020-03-15 22:16:54 +0100

    Ajout d'une classe permettant de calculer les points a partir d'un mot entré en parametre

---
+++ src/CalculPoint.java
@@ -0,0 +1,29 @@
+
+
+public class CalculPoint extends DictionnairePoint {
+	
+	String mot;
+	static int point;
+	DictionnairePoint liste = new DictionnairePoint();
+	
+	public CalculPoint(String m) {
+		this.mot = m;
+		for(int i=0; i<m.length();i++) {
+			String s = Character.toString(m.charAt(i));
+			point+=getvalue(s);
+		}
+	}
+	
+	public static int Point() {
+		return point;
+	}
+	
+
+	public static void main(String[] args) {
+		
+		new CalculPoint("youtube");
+		System.out.println(Point());
+
+	}
+
+}
