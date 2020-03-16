b8c51d4c04aad634ab79310e178a37e04b0fee3a
loic-dev <loic.charrie.12@gmail.com>
2020-03-16 15:30:19 +0100

    ajout d'une classe qui construit une matrice representant la plateau vide avec cases qui double et triple et une autre matrice representant l'etat du jeu en binaire

---
+++ src/MatricePlateau.java
@@ -0,0 +1,150 @@
+
+public class MatricePlateau {
+	
+	static int[][] matrice = new int[15][15];
+	static int[][] matriceModify = new int[15][15];
+	static String error ="pas d'erreur";
+	
+	
+	public MatricePlateau() {
+		for(int i = 0; i < matrice.length; i++){
+	        matrice[i] = new int[15];
+	    }
+		MotTriple();
+		MotDouble();
+		LettreTriple();
+		LettreDouble();
+	}
+	
+	public static void MotTriple() {
+		for(int i = 0; i < matrice.length; i++){
+            for(int j = 0; j < matrice[i].length; j++){
+            	if(i==0 && j==0 || i==matrice.length-1 && j==matrice.length-1 || i==0 && j==matrice.length-1 || i==matrice.length-1 && j==0) {
+            		matrice[i][j] = 9;
+            	}
+            	else if(i==0 && j==(matrice.length-1)/2 || i==matrice.length-1 && j==(matrice.length-1)/2) {
+            		matrice[i][j] = 9;
+            	}
+            	else if(j==0 && i==(matrice.length-1)/2 || j==matrice.length-1 && i==(matrice.length-1)/2) {
+            		matrice[i][j] = 9;
+            	}
+            	
+            }
+       }
+	}
+	
+	public static void MotDouble() {
+		for(int i = 0; i < matrice.length; i++){
+            for(int j = 0; j < matrice[i].length; j++){
+            	if(i==j && i>0 && i<5) {
+            		matrice[i][j] = 6;
+            	}
+            	else if(i==j && i>8 && i<matrice.length-1) {
+            		matrice[i][j] = 6;
+            	}
+            	else if(i>8 && i<matrice.length-1 && j==matrice.length-i-1 ) {
+            		matrice[i][j] = 6;
+            	}
+            	else if(i>0 && i<5 && j==matrice.length-i-1 ) {
+            		matrice[i][j] = 6;
+            	}
+            }
+       }
+	}
+	
+	public static void LettreTriple() {
+		for(int i = 0; i < matrice.length; i++){
+            for(int j = 0; j < matrice[i].length; j++){
+            	if(i==5 && j==5 || i==9 && j==9 || i==5 && j==9 || i==9 && j==5 ) {
+            		matrice[i][j] = 3;
+            	}
+            	else if((i==5 || i==9) && (j==1 || j==matrice.length-2)) {
+            		matrice[i][j] = 3;
+            	}
+            	else if((j==5 || j==9) && (i==1 || i==matrice.length-2)) {
+            		matrice[i][j] = 3;
+            	}
+            }
+       }
+	}
+	
+	
+	public static void LettreDouble() {
+		for(int i = 0; i < matrice.length; i++){
+            for(int j = 0; j < matrice[i].length; j++){
+            	if(i==6 && j==6 || i==8 && j==8 || i==6 && j==8 || i==8 && j==6 ) {
+            		matrice[i][j] = 2;
+            	}
+            	else if((i==6 || i==8) && (j==2 || j==matrice.length-3)) {
+            		matrice[i][j] = 2;
+            	}
+            	else if((j==6 || j==8) && (i==2 || i==matrice.length-3)) {
+            		matrice[i][j] = 2;
+            	}
+            	else if(i==(matrice.length-1)/2 && j==3 || i==(matrice.length-1)/2 && j==matrice.length-4 ) {
+            		matrice[i][j] = 2;
+            	}
+            	else if(j==(matrice.length-1)/2 && i==3 || j==(matrice.length-1)/2 && i==matrice.length-4 ) {
+            		matrice[i][j] = 2;
+            	}
+            	else if(j==(matrice.length-1)/4 && i==0 || j==(matrice.length)*3/4 && i==0) {
+            		matrice[i][j] = 2;
+            	}
+            	else if(j==(matrice.length-1)/4 && i==matrice.length-1 || j==(matrice.length)*3/4 && i==matrice.length-1) {
+            		matrice[i][j] = 2;
+            	}
+            	
+            	else if(i==(matrice.length-1)/4 && j==0 || i==(matrice.length)*3/4 && j==0) {
+            		matrice[i][j] = 2;
+            	}
+            	else if(i==(matrice.length-1)/4 && j==matrice.length-1 || i==(matrice.length)*3/4 && j==matrice.length-1) {
+            		matrice[i][j] = 2;
+            	}
+            }
+       }
+	}
+	
+	public static void matriceModify() {
+		for(int i = 0; i < matriceModify.length; i++){
+	        matriceModify[i] = new int[15];
+	    }
+	}
+	
+	
+	
+	public static void addlettre(int x,int y) {
+		if(matriceModify[y][x]==0) {
+	        matriceModify[y][x]+=1;
+		}
+		else {
+			error = "case deja utilisé";
+		}
+		
+        
+    }
+		
+		
+	
+	
+	public static void main(String[] args) {
+		new MatricePlateau();
+		addlettre(5,5);
+		
+		for(int i = 0; i < matrice.length; i++){
+            for(int j = 0; j < matrice[i].length; j++){
+                System.out.print(matrice[i][j] + " ");
+            }
+            System.out.println();
+        }
+		System.out.println();
+		for(int i = 0; i < matriceModify.length; i++){
+            for(int j = 0; j < matriceModify[i].length; j++){
+                System.out.print(matriceModify[i][j] + " ");
+            }
+            System.out.println();
+        }
+		System.out.println();
+		System.out.println(error);
+	}
+
+}
