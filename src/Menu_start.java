a92123d1d9afc306f9b63901f3773e85913742f8
loic-dev <loic.charrie.12@gmail.com>
2020-03-17 21:26:09 +0100

    ajout d'un menu de demarrage qui permet de lancer une partie

---
+++ src/Menu_start.java
@@ -0,0 +1,57 @@
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Menu_start implements ActionListener  {
+	
+	static JPanel menu = new JPanel();
+	static JButton button = new JButton("Nouvelle partie");
+	static JButton button2 = new JButton("Chargé partie");
+	
+
+	
+	public Menu_start() {
+		button.addActionListener(this);
+		button2.addActionListener(this);
+		menu.add(button);
+		menu.add(button2);     
+	}
+	
+	public JPanel GetMenu() {
+		return menu;
+	}
+	
+	public static JButton Getbutton(int i) {
+		if(i==0) {
+			return button;
+		}
+		else {
+			return button2;
+		}
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if(source == Menu_start.Getbutton(0)){
+			Vue.removeMenu();
+			Vue.jeu();
+			new Vue();
+		}
+		
+	}
+	
+	
+
+	
+	
+	
+
+}
+
+
