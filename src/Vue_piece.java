8f41f1c2ab827e5ff7d2fc9e236e4b7061a22f68
loic-dev <loic.charrie.12@gmail.com>
2020-03-17 15:31:02 +0100

    classe de la vue qui affiche les 7 pieces tirés au hasard

---
+++ src/Vue_piece.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Vue_piece {
+	
+	static JPanel piece = new JPanel();
+	PieceHasard liste = new PieceHasard();
+	String h[] = liste.tirage();
+
+	
+	public Vue_piece() {
+		
+		
+		 piece.setLayout(new GridLayout(7,1));
+		 piece.setPreferredSize(new Dimension(50,200));
+	        
+	        for(int i=0;i<7;i++) {
+	        	JButton button = new JButton(h[i]);
+	        	button.setBackground(Color.orange);
+            	button.setOpaque(true);
+            	button.setBorderPainted(true);
+            	piece.add(button);
+	        	}
+	        }
+
+	
+	public JPanel GetVuePiece() {
+		return piece;
+	}
+
+}
