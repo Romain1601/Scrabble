2ce674e74dc4c9c550a501dc4c24ca78e75ab932
loic-dev <loic.charrie.12@gmail.com>
2020-03-17 15:32:52 +0100

    update de classe qui affiche la vue globale

--- src/Vue.java
+++ src/Vue.java
@@ -1,65 +1,27 @@
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+
 
 public class Vue extends JFrame {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	JPanel plateau= new JPanel();
-	JPanel dashboard= new JPanel();
+	
+	Vue_Plateau plateau = new Vue_Plateau();
+	Vue_piece piece = new Vue_piece();
+	
 	
 	public Vue() {
-    	
-    	new MatricePlateau();
-    	int g[][] = MatricePlateau.GetMatrice();
- 
-    	JFrame fenetre = new JFrame();            
+		
+		
+		plateau.GetVuePlateau();
+		JFrame fenetre = new JFrame();            
 		fenetre.setTitle("Scrabble");
 		fenetre.setSize(600, 600);
 		fenetre.setLocationRelativeTo(null); 
         fenetre.setResizable(true);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenetre.setLayout(new FlowLayout());
-        fenetre.add(plateau);
-        
-        
-       
-        plateau.setLayout(new GridLayout(15,15));
-        plateau.setPreferredSize(new Dimension(400,400));
-        
-        for(int i=0;i<15;i++) {
-        	for(int j=0;j<15;j++) {
-        		JButton button = new JButton();
-        		if(g[i][j] == 9) {
-        			button.setBackground(Color.red);
-        		}
-        		else if(g[i][j] == 6) {
-        			button.setBackground(Color.pink);
-        		}
-        		else if(g[i][j] == 3) {
-        			button.setBackground(Color.blue);
-        		}
-        		else if(g[i][j] == 2) {
-        			button.setBackground(Color.cyan);
-        		}
-        		else {
-        			button.setBackground(Color.green);
-        		}
-            	button.setOpaque(true);
-            	button.setBorderPainted(true);
-            	plateau.add(button);
-        	}
-        	
-        	
-        	
-        }
+        fenetre.add(plateau.GetVuePlateau());
+        fenetre.add(piece.GetVuePiece());
         fenetre.setVisible(true); 
     }
      
