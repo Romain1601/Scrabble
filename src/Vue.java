b8469f7fdd660fc0311908b652cb1bea2be5d7f4
loic-dev <loic.charrie.12@gmail.com>
2020-03-17 21:25:11 +0100

    update vue

--- src/Vue.java
+++ src/Vue.java
@@ -2,34 +2,62 @@ import java.awt.FlowLayout;
 import javax.swing.JFrame;
 
 
-public class Vue extends JFrame {
+
+public class Vue extends JFrame    {
 	private static final long serialVersionUID = 1L;
 	
-	Vue_Plateau plateau = new Vue_Plateau();
-	Vue_piece piece = new Vue_piece();
+	static Vue_Plateau plateau = new Vue_Plateau();
+	static Vue_piece piece = new Vue_piece();
+	static Menu_start menu = new Menu_start();
+	static JFrame fenetre = new JFrame(); 
+	
 	
 	
 	public Vue() {
-		
-		
-		plateau.GetVuePlateau();
-		JFrame fenetre = new JFrame();            
+		plateau.GetVuePlateau();       
 		fenetre.setTitle("Scrabble");
-		fenetre.setSize(600, 600);
+		fenetre.setSize(450, 600);
 		fenetre.setLocationRelativeTo(null); 
         fenetre.setResizable(true);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenetre.setLayout(new FlowLayout());
-        fenetre.add(plateau.GetVuePlateau());
-        fenetre.add(piece.GetVuePiece());
         fenetre.setVisible(true); 
+        
+   
+      
+        
     }
-     
+	
+	
+	public static void menu() {
+		fenetre.add(menu.GetMenu());
+	}
+	
+	public static void removeMenu() {
+		fenetre.remove(menu.GetMenu());
+	}
+	
+	public static void jeu() {
+		fenetre.add(plateau.GetVuePlateau());
+        fenetre.add(piece.GetVuePiece());
+	}
+	
+	
+	
+
     public static void main(String[] args) {
-    
+    	menu();
         new Vue();
+       
    }
 
+
+
+
+
+	
+
+	
 	
 	
 	
