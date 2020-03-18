2c24e18005b6c4c72404baeffba3bb4b2eb60058
loic-dev <loic.charrie.12@gmail.com>
2020-03-18 23:05:00 +0100

    update Button Plateau

--- src/ButtonPlateau.java
+++ src/ButtonPlateau.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import javax.swing.JButton;
 
 
@@ -13,7 +14,39 @@ public class ButtonPlateau {
 		this.y = y;
         this.name = name;
         button = new JButton(name);
+        button.setOpaque(true);
+        button.setBorderPainted(true);
 	}
+	
+	public void ButtonRed() {
+		button.setBackground(Color.RED);
+	}
+	
+	public void ButtonPink() {
+		button.setBackground(Color.PINK);
+	}
+	
+	public void ButtonBlue() {
+		button.setBackground(Color.BLUE);
+	}
+	
+	public void ButtonCyan() {
+		button.setBackground(Color.CYAN);
+	}
+	
+	public void ButtonGreen() {
+		button.setBackground(Color.GREEN);
+	}
+	
+	public void ButtonORANGE() {
+		button.setBackground(Color.ORANGE);
+	}
+	
+	public void setlabelButton(String i) {
+		button.setText(i);
+	}
+	
+	
 	
 	public JButton GetButton() {
         return button;
