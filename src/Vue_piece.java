7a5d2b29534b0d8b5081e632511046117146f11b
loic-dev <loic.charrie.12@gmail.com>
2020-03-18 23:08:14 +0100

    update vue des pieces

--- src/Vue_piece.java
+++ src/Vue_piece.java
@@ -1,44 +1,59 @@
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
 public class Vue_piece implements ActionListener {
 	
 	static JPanel piece = new JPanel();
-	PieceHasard liste = new PieceHasard();
-	String h[] = liste.tirage();
-	static ButtonPiece b[] = new ButtonPiece[7];
-	
+	static ArrayList<ButtonPiece> b = new ArrayList<ButtonPiece>();
+	static int k=1;
+	static int index;
 	
 	public Vue_piece() {
-		
-		 piece.setLayout(new GridLayout(1,7));
-		 piece.setPreferredSize(new Dimension(350,40));
-	        for(int i=0;i<7;i++) {
-	        	b[i] = new ButtonPiece(i,h[i]);
-	        	b[i].GetButton().setBackground(Color.orange);
-	        	b[i].GetButton().setOpaque(true);
-	        	b[i].GetButton().setBorderPainted(true);
-	        	b[i].GetButton().addActionListener(this);
-            	piece.add(b[i].GetButton());
+		if(k==1) {
+			new PieceHasard();
+		}
+		piece.setLayout(new GridLayout(1,PieceHasard.getsizedepart()));
+		piece.setPreferredSize(new Dimension(350,40));
+	        for(int i=0;i<PieceHasard.getsizedepart();i++) {
+	        	b.add(new ButtonPiece(i,PieceHasard.getdepart(i)));
+	        	b.get(i).GetButton().addActionListener(this);
+            	piece.add(b.get(i).GetButton());
 	        	}
 	        }
+	
 	
 	public JPanel GetVuePiece() {
 		return piece;
 	}
 	
+	public static void removeButton(String i) {
+		index = PieceHasard.indexListe(i);
+		b.remove(index);
+		
+			
+	}
 	
-
+	
+	public static void removePiece(String name){
+		removeButton(name);
+		PieceHasard.RemoveListe(index);
+		piece.removeAll();	
+		k=2;
+		new Vue_piece();
+	}
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
-		for(int i=0;i<7;i++) {
-			if(source == b[i].GetButton()) {
-				System.out.println(b[i].GetNameButton());
+		for(int i=0;i<PieceHasard.getsizedepart();i++) {
+			if(source == b.get(i).GetButton()) {
+				RecupPiece.changePiece(b.get(i).GetNameButton());
 			}	
 		}
 		 
