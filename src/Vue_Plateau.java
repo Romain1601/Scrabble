2a0c366b2db64897c269a26fdf6871a51b27a2a8
loic-dev <loic.charrie.12@gmail.com>
2020-03-18 23:09:23 +0100

    update vue plateau

--- src/Vue_Plateau.java
+++ src/Vue_Plateau.java
@@ -1,44 +1,52 @@
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 
 import javax.swing.JPanel;
 
 public class Vue_Plateau implements ActionListener {
 	
 	static JPanel plateau = new JPanel();
-	MatricePlateau matrice = new MatricePlateau();
-	int g[][] = matrice.GetMatrice();
+	int g[][] = MatricePlateau.GetMatrice();
+	int o[][] = MatricePlateau.GetMatriceModify();
 	static ButtonPlateau b[][] = new ButtonPlateau[15][15];
+	static int k=1;
 
 	
 	public Vue_Plateau() {
+		if(k==1) {
+			new MatricePlateau();
+		}
 		 plateau.setLayout(new GridLayout(15,15));
-	     plateau.setPreferredSize(new Dimension(400,400));
-	        
+	     plateau.setPreferredSize(new Dimension(700,600));
+	     MatricePlateau.afficherMatriceModify() ;
 	        for(int i=0;i<15;i++) {
 	        	for(int j=0;j<15;j++) {
+	        		
 	        		b[i][j] = new ButtonPlateau(i,j,"");
 	        		if(g[i][j] == 9) {
-	        			b[i][j].GetButton().setBackground(Color.red);
+	        			b[i][j].ButtonRed();
 	        		}
 	        		else if(g[i][j] == 6) {
-	        			b[i][j].GetButton().setBackground(Color.pink);
+	        			b[i][j].ButtonPink();
 	        		}
 	        		else if(g[i][j] == 3) {
-	        			b[i][j].GetButton().setBackground(Color.blue);
+	        			b[i][j].ButtonBlue();
 	        		}
 	        		else if(g[i][j] == 2) {
-	        			b[i][j].GetButton().setBackground(Color.cyan);
+	        			b[i][j].ButtonCyan();
 	        		}
+					
 	        		else {
-	        			b[i][j].GetButton().setBackground(Color.green);
+	        			b[i][j].ButtonGreen();
 	        		}
-	        		b[i][j].GetButton().setOpaque(true);
+	        		if(o[j][i] == 1) {
+	        			b[i][j].setlabelButton(RecupPiece.Pieceselect());
+	        			b[i][j].ButtonORANGE();
+	        		}
 	        		b[i][j].GetButton().addActionListener(this);
-	        		b[i][j].GetButton().setBorderPainted(true);
 	            	plateau.add(b[i][j].GetButton());
 	        	}
 	        }
@@ -47,6 +55,13 @@ public class Vue_Plateau implements ActionListener {
 	public JPanel GetVuePlateau() {
 		return plateau;
 	}
+	
+	
+	public static void addPiecePlateau(){
+		plateau.removeAll();
+		new Vue_Plateau();
+		
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -54,8 +69,12 @@ public class Vue_Plateau implements ActionListener {
 		for(int i=0;i<15;i++) {
 			for(int j=0;j<15;j++) {
 				if(source == b[i][j].GetButton()) {
-					System.out.println(b[i][j].GetxButton() +","+ b[i][j].GetyButton());
-					
+					DictionnairePiece.removeletter(RecupPiece.Pieceselect());
+					Vue_piece.removePiece(RecupPiece.Pieceselect());
+					MatricePlateau.addlettre(b[i][j].GetxButton(),b[i][j].GetyButton());
+					addPiecePlateau();
+					new Vue();
+				
 				}	
 			}
 			
