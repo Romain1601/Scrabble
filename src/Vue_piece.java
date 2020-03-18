c6318ab4c67bc8ac35c93eb04e3e1dcf709f8372
loic-dev <loic.charrie.12@gmail.com>
2020-03-18 14:56:26 +0100

    update vue_piece

--- src/Vue_piece.java
+++ src/Vue_piece.java
@@ -1,35 +1,49 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
-public class Vue_piece {
+public class Vue_piece implements ActionListener {
 	
 	static JPanel piece = new JPanel();
 	PieceHasard liste = new PieceHasard();
 	String h[] = liste.tirage();
-
+	static ButtonPiece b[] = new ButtonPiece[7];
+	
 	
 	public Vue_piece() {
 		
-		
 		 piece.setLayout(new GridLayout(1,7));
 		 piece.setPreferredSize(new Dimension(350,40));
-	        
 	        for(int i=0;i<7;i++) {
-	        	JButton button = new JButton(h[i]);
-	        	button.setBackground(Color.orange);
-            	button.setOpaque(true);
-            	button.setBorderPainted(true);
-            	piece.add(button);
+	        	b[i] = new ButtonPiece(i,h[i]);
+	        	b[i].GetButton().setBackground(Color.orange);
+	        	b[i].GetButton().setOpaque(true);
+	        	b[i].GetButton().setBorderPainted(true);
+	        	b[i].GetButton().addActionListener(this);
+            	piece.add(b[i].GetButton());
 	        	}
 	        }
-
 	
 	public JPanel GetVuePiece() {
 		return piece;
 	}
+	
+	
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		for(int i=0;i<7;i++) {
+			if(source == b[i].GetButton()) {
+				System.out.println(b[i].GetNameButton());
+			}	
+		}
+		 
+			
+		
+	}
+	
 }
