51790f202f689260018a27b0ab42960f867cd6d4
loic-dev <loic.charrie.12@gmail.com>
2020-03-19 23:33:14 +0100

    update vue

--- src/Vue.java
+++ src/Vue.java
@@ -7,6 +7,7 @@ public class Vue extends JFrame    {
 	private static final long serialVersionUID = 1L;
 	
 	static Vue_Plateau plateau = new Vue_Plateau();
+	static PiocherLetter valider = new PiocherLetter("valider");
 	static Vue_piece piece = new Vue_piece();
 	static Menu_start menu = new Menu_start();
 	static JFrame fenetre = new JFrame(); 
@@ -35,6 +36,11 @@ public class Vue extends JFrame    {
 	public static void jeu() {
 		fenetre.add(plateau.GetVuePlateau());
         fenetre.add(piece.GetVuePiece());
+	}
+	
+	public static void addvalider() {
+		fenetre.add(valider.GetPiocher());
+        
 	}
 
 	
