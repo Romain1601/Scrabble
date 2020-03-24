efc65f46687d9638d9cb229dc15f39f84f62c4bb
loic-dev <loic.charrie.12@gmail.com>
2020-03-24 16:37:48 +0100

    ajout de la vue du plateau

--- src/com/sdz/vue/Vue_Plateau.java
+++ src/com/sdz/vue/Vue_Plateau.java
@@ -1,114 +1,110 @@
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
+package com.sdz.vue;
+import java.awt.Color;
 
+import java.awt.GridLayout;
+
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Vue_Plateau implements ActionListener,MouseListener {
-	
-	static JPanel plateau = new JPanel();
-	static ButtonPlateau b[][] = new ButtonPlateau[15][15];
-	static ArrayList<String> u = new ArrayList<String>();
-	int c;
-	static int k=1;
+import com.sdz.controler.Controleur_Plateau;
+import com.sdz.model.ListeJoueurs;
+import com.sdz.model.Model_Plateau;
+import com.sdz.observer.Observer;
 
+
+public class Vue_Plateau extends JPanel implements Observer {
 	
-	public Vue_Plateau() {
-		if(k==1) {
-			new MatricePlateau();
-		}
-		 plateau.setLayout(new GridLayout(15,15));
-	     plateau.setPreferredSize(new Dimension(700,600));
-	        for(int i=0;i<15;i++) {
-	        	for(int j=0;j<15;j++) {
-	        		
-	        		b[i][j] = new ButtonPlateau(i,j,"");
-	        		if(MatricePlateau.GetMatrice()[i][j] == 9) {
-	        			b[i][j].ButtonRed();
-	        		}
-	        		else if(MatricePlateau.GetMatrice()[i][j] == 6) {
-	        			b[i][j].ButtonPink();
-	        		}
-	        		else if(MatricePlateau.GetMatrice()[i][j] == 3) {
-	        			b[i][j].ButtonBlue();
-	        		}
-	        		else if(MatricePlateau.GetMatrice()[i][j] == 2) {
-	        			b[i][j].ButtonCyan();
-	        		}
-					
-	        		else {
-	        			b[i][j].ButtonWhite();
-	        		}
-	        		if(MatricePlateau.GetMatriceModify()[j][i]==1) {
-	        			b[i][j].GetButton().addMouseListener(this);
-	        			if(MatricePlateau.GetMatriceEtat()[j][i] == RecupPiece.Pieceselect()) {
-	        				b[i][j].setlabelButton(RecupPiece.Pieceselect());
-		        			b[i][j].ButtonORANGE();
-	        			}else {
-	        				
-	        				b[i][j].setlabelButton(MatricePlateau.GetMatriceEtat()[j][i]);
-		        			b[i][j].ButtonORANGE();
-	        			}
-	        		}
-	        		else if(MatricePlateau.GetMatriceModify()[j][i]==2) {
-	        				b[i][j].GetButton().addMouseListener(this);
-	        				b[i][j].setlabelButton(MatricePlateau.GetMatriceEtat()[j][i]);
-		        			b[i][j].ButtonYellow();
-	        			
-	        		}
-	        		b[i][j].GetButton().addActionListener(this);
-	            	plateau.add(b[i][j].GetButton());
-	        	}
-	        }
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public JPanel plateau = new JPanel();
+	public JButton grilleButton[][] = new JButton[15][15];
+	Model_Plateau ModelPlateau = new Model_Plateau();
+	Controleur_Plateau controleur;
+	ListeJoueurs listeJoueurs;
+	int NumJoueur;
+	
+
+	public Vue_Plateau(ListeJoueurs l, int num) {
+		this.listeJoueurs=l;
+		this.NumJoueur = num;
+		controleur = new Controleur_Plateau(this, ModelPlateau, listeJoueurs, NumJoueur);
+		ModelPlateau.addObserver(this);
+		plateau.setLayout(new GridLayout(15,15));
+		plateau.setBounds(20,20,650,600);
+		this.ConstruireVuePlateau();
 	}
 	
 	public JPanel GetVuePlateau() {
 		return plateau;
 	}
 	
-	
-	public static void updatePlateau(){
-		plateau.removeAll();
-		new Vue_Plateau();
-		
+	public JButton GetButtonPlateau(int x,int y) {
+		return grilleButton[x][y];
 	}
+	
+	
+	public void ConstruireVuePlateau() {
+	        for(int i=0;i<15;i++) {
+	        	for(int j=0;j<15;j++) {
+	        		grilleButton[i][j]=new JButton();
+	        		grilleButton[i][j].addMouseListener(controleur);
+	        		switch(ModelPlateau.getMatriceEtat()[j][i]) {
+	        		 
+	        		case 0:
+	        			switch(ModelPlateau.getMatriceStatique()[j][i]) {
+	        			case 9:
+	        				grilleButton[i][j].setBackground(Color.red);
+	        				break;
+	        			case 6:
+	        				grilleButton[i][j].setBackground(Color.pink);
+	        				break;
+	        			case 3:
+	        				grilleButton[i][j].setBackground(Color.blue);
+	        				break;
+	        			case 2:
+	        				grilleButton[i][j].setBackground(Color.cyan);
+	        				break;
+	        			case 0:
+	        				grilleButton[i][j].setBackground(Color.white);
+	        				break;
+	        			}
+	        			break;
+	        		case 1:
+	        			if(ModelPlateau.getMatriceLettre()[j][i] == ModelPlateau.getLettreSelect()) {
+	        				grilleButton[i][j].setText(ModelPlateau.getLettreSelect());
+		        			grilleButton[i][j].setBackground(Color.orange);
+	        			}
+	        			else {
+	        				grilleButton[i][j].setText(ModelPlateau.getMatriceLettre()[j][i]);
+	        				grilleButton[i][j].setBackground(Color.orange);
+	        			}
+	        			break;
+	        		case 2:
+	        			grilleButton[i][j].setText(ModelPlateau.getMatriceLettre()[j][i]);
+	        			grilleButton[i][j].setBackground(Color.yellow);
+	        			
+	        		}
+	            	plateau.add(grilleButton[i][j]);
+	        	}
+	        }
+	}
+	
+	
 
 
-	
-	public static void add(String n) {
-		u.add(n);
-	}
-	
-	public static void remove(String n) {
-		u.remove(n);
-	}
-	
-	public static boolean ifexist(String n) {
-		for(int i=0; i<u.size();i++) {
-			String l = u.get(i);
-			if(l==n) {
-				return true;
-				}
-			}
-		
-		return false;
-	}
-	
-	public static ArrayList<String> getlist() {
-		return u;
-	}
 
-	@Override
+	/*@Override
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
 		for(int i=0;i<15;i++) {
 			for(int j=0;j<15;j++) {
 				if(source == b[i][j].GetButton()) {
-					if(RecupPiece.Pieceselect()!=null && MatricePlateau.GetMatriceModify()[j][i]==0) {
+					if(RecupPiece.Pieceselect()!=null && MatricePlateau.getMatriceEtat()[j][i]==0) {
 						Vue_piece.removePiece(RecupPiece.Pieceselect());
 						MatricePlateau.addlettre(b[i][j].GetxButton(),b[i][j].GetyButton(),RecupPiece.Pieceselect());
 						updatePlateau();
@@ -141,14 +137,14 @@ public class Vue_Plateau implements ActionListener,MouseListener {
 		for(int i=0;i<15;i++) {
 			for(int j=0;j<15;j++) {
 		if(buttonDown == MouseEvent.BUTTON3 && source == b[i][j].GetButton()) {
-			if(MatricePlateau.GetMatriceModify()[j][i]==1) {
+			if(MatricePlateau.getMatriceEtat()[j][i]==1) {
 				Vue_piece.add(MatricePlateau.GetMatriceEtat()[j][i]);
 				MatricePlateau.removelettre(b[i][j].GetxButton(),b[i][j].GetyButton());
 				updatePlateau();
 				new Vue_piece();
 				new Vue();
 			}
-			else if(MatricePlateau.GetMatriceModify()[j][i]==2) {
+			else if(MatricePlateau.getMatriceEtat()[j][i]==2) {
 				remove(MatricePlateau.GetMatriceEtat()[j][i]);
 				System.out.println(u);
 				MatricePlateau.unselectlettre(b[i][j].GetxButton(),b[i][j].GetyButton());
@@ -160,7 +156,7 @@ public class Vue_Plateau implements ActionListener,MouseListener {
 			
 	    }
 		if(buttonDown == MouseEvent.BUTTON1 && source == b[i][j].GetButton()){
-			if(MatricePlateau.GetMatriceModify()[j][i]==1) {
+			if(MatricePlateau.getMatriceEtat()[j][i]==1) {
 				add(MatricePlateau.GetMatriceEtat()[j][i]);
 				System.out.println(u);
 				MatricePlateau.selectlettre(b[i][j].GetxButton(),b[i][j].GetyButton());
@@ -191,7 +187,22 @@ public class Vue_Plateau implements ActionListener,MouseListener {
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
+	}*/
+
+	
+
+
+	@Override
+	public void update(String str) {
+		plateau.removeAll();
+		ConstruireVuePlateau();
+		plateau.updateUI();;
+	
 	}
+
+
+
+	
 
 
 }
