92a2e34b569f983bde0e0ee86431a3351cd6dd52
loic-dev <loic.charrie.12@gmail.com>
2020-03-24 16:38:29 +0100

    update de la vue de la fenetre globale

--- src/com/sdz/vue/Vue.java
+++ src/com/sdz/vue/Vue.java
@@ -1,31 +1,82 @@
-import java.awt.FlowLayout;
+package com.sdz.vue;
+
+
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import com.sdz.model.ListeJoueurs;
+import com.sdz.model.Model_Pioche;
+import com.sdz.vue.Vue_Plateau;
 
 
-public class Vue extends JFrame    {
+
+
+
+
+public class Vue extends JFrame   {
 	private static final long serialVersionUID = 1L;
 	
-	static Vue_Plateau plateau = new Vue_Plateau();
-	static PiocherLettre Piocher = new PiocherLettre();
+
+	
+	Model_Pioche modelPioche = new Model_Pioche(); 
+	ListeJoueurs listeJoueurs = new ListeJoueurs();
+	Vue_Chevalet_Joueur Vue_chevalet_joueur = new Vue_Chevalet_Joueur(1, modelPioche, listeJoueurs);
+	Vue_Plateau vue_plateau = new Vue_Plateau(listeJoueurs, 1);
+	Vue_Pioche vue_pioche = new Vue_Pioche(modelPioche);
+	
+	//Controleur_Plateau c = new Controleur_Plateau(vue_plateau,Model_plateau);
+	/*static PiocherLettre Piocher = new PiocherLettre();
 	static Valider_mot Valider = new Valider_mot();
 	static Vue_piece piece = new Vue_piece();
-	static Menu_start menu = new Menu_start();
-	static JFrame fenetre = new JFrame(); 
+	static Menu_start menu = new Menu_start();*/
+	JFrame fenetre = new JFrame(); 
 	
 	public Vue() {
-		plateau.GetVuePlateau();       
 		fenetre.setTitle("Scrabble");
-		fenetre.setSize(800, 1000);
+		fenetre.setSize(1000, 1000);
 		fenetre.setLocationRelativeTo(null); 
-        fenetre.setResizable(true);
+        fenetre.setResizable(false);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetre.setLayout(new FlowLayout());
-        fenetre.setVisible(true);    
+        fenetre.setLayout(null);
+        fenetre.setVisible(true);
+        fenetre.add(vue_plateau.GetVuePlateau());
+        fenetre.add(vue_pioche.GetVuePioche());
+        fenetre.add(Vue_chevalet_joueur.GetVueNomJoueur());
+        fenetre.add(Vue_chevalet_joueur.GetVueChevaletJoueur());
+        fenetre.add(Vue_chevalet_joueur.GetVueChevaletBoutton());
+        
+        
     }
 	
 	
-	public static void menu() {
+	/*public void addPlateauMouseListener() {
+		 for(int i=0;i<15;i++) {
+				for(int j=0;j<15;j++) {
+					vue_plateau.GetButtonPlateau(i,j).addMouseListener(c);
+					
+				}
+		 }
+	}*/
+	
+	
+	
+	public JFrame getVue() {
+		return fenetre;
+	}
+	
+	
+	public JButton getbuttonsPlateau(int x,int y) {
+		return vue_plateau.GetButtonPlateau(x,y);
+	}
+
+
+	
+	
+	
+	
+	
+	/*public static void menu() {
 		fenetre.add(menu.GetMenu());
 	}
 	
@@ -44,16 +95,12 @@ public class Vue extends JFrame    {
 	
 	public static void addButtonValider() {
 		fenetre.add(Valider.Getvalider());  
-	}
+	}*/
 
 	
-    public static void main(String[] args) {
-    	menu();
-        new Vue();
-       
-       
-   }
-
+  
+	
+	
 
 
 
