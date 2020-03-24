f93ad4a911391b39b1207794d0f79a8aac298954
loic-dev <loic.charrie.12@gmail.com>
2020-03-24 16:20:48 +0100

    ajout du controleur du chevalet

---
+++ src/com/sdz/controler/Controleur_Chevalet.java
@@ -0,0 +1,110 @@
+package com.sdz.controler;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+
+import com.sdz.model.ListeJoueurs;
+import com.sdz.model.Model_Pioche;
+
+import com.sdz.vue.Vue_Chevalet_Joueur;
+
+
+
+public class Controleur_Chevalet implements MouseListener, ActionListener {
+	
+	Vue_Chevalet_Joueur vue;
+	Model_Pioche modelPioche;
+	ListeJoueurs listeJoueurs;
+	ArrayList<String> listLettre;
+	
+	
+	public Controleur_Chevalet(Vue_Chevalet_Joueur v, Model_Pioche m, ListeJoueurs l) {
+		this.vue=v;
+		this.modelPioche=m;
+		this.listeJoueurs=l;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		 
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		Object source = e.getSource();
+		
+		int buttonDown = e.getButton();
+		System.out.println(buttonDown);
+		for(int i=0;i<listeJoueurs.GetsizeChevalet(vue.GetNumJoueur());i++) {
+			if(buttonDown == MouseEvent.BUTTON1) {
+				if(e.isControlDown() && source == vue.getListeButtonChevalet().get(i).GetButton()) {
+					listeJoueurs.changeChevaletEtat(vue.GetNumJoueur(),i,2);
+					vue.GetButtonChangerLettre().setVisible(true);
+			    }
+				else if(source == vue.getListeButtonChevalet().get(i).GetButton()) {
+					listeJoueurs.changeChevaletEtat(vue.GetNumJoueur(),i,1);
+					
+					//changerLettre.setEnabled(false);
+				}
+			}
+			else if(buttonDown == MouseEvent.BUTTON3) {
+				if(source == vue.getListeButtonChevalet().get(i).GetButton()) {
+					listeJoueurs.changeChevaletEtat(vue.GetNumJoueur(),i,0);
+				}
+			}
+			
+		
+		
+	}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if(source == vue.GetButtonPiocher()) {
+			if(listeJoueurs.GetChevalet(vue.GetNumJoueur()).size()<7){
+				listeJoueurs.addLettreHasardChevalet(vue.GetNumJoueur());
+				ArrayList<String> lettre = listeJoueurs.GetChevalet(vue.GetNumJoueur());
+				modelPioche.removeLettre(lettre);
+			}
+			
+		}
+		else if(source == vue.GetButtonChangerLettre()) {
+			modelPioche.addLettre(vue.getListeLettreChevaletselectGreen());
+			listeJoueurs.removeListLettreChevalet(vue.GetNumJoueur(),vue.getListeLettreChevaletselectGreen());
+			for(int i=0;i<vue.getListeLettreChevaletselectGreen().size();i++) {
+				listeJoueurs.changeChevaletEtat(vue.GetNumJoueur(),i,0);	
+			}
+			vue.GetButtonChangerLettre().setVisible(false);
+			
+			
+			
+			
+		}
+		
+		
+	}
+	
+}
