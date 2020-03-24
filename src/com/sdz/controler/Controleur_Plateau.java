f99b9b7e907c32d56b26f21e365eb9259d66c04a
loic-dev <loic.charrie.12@gmail.com>
2020-03-24 16:21:58 +0100

    Ajout du controleur du plateau

---
+++ src/com/sdz/controler/Controleur_Plateau.java
@@ -0,0 +1,92 @@
+package com.sdz.controler;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import com.sdz.model.ListeJoueurs;
+import com.sdz.model.Model_Plateau;
+import com.sdz.vue.Vue_Plateau;
+
+public class Controleur_Plateau implements MouseListener {
+	Vue_Plateau vue;
+	Model_Plateau model;
+	ListeJoueurs listeJoueurs;
+	int NumJoueur;
+	String lettreselect; 
+	
+	public Controleur_Plateau(Vue_Plateau v, Model_Plateau m, ListeJoueurs l, int num) {
+		this.vue=v;
+		this.model=m;
+		this.listeJoueurs=l;
+		this.NumJoueur=num;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		int clic = e.getButton();
+		Object source = e.getSource();
+		int[] etat = listeJoueurs.GetChevaletEtat(NumJoueur);
+		if(clic == MouseEvent.BUTTON1){
+			for(int k=0;k<etat.length;k++) {
+				if(etat[k]==1) {
+					lettreselect = listeJoueurs.GetlettreChevalet(NumJoueur,k);
+					listeJoueurs.removeLettreChevalet(NumJoueur,lettreselect);
+					listeJoueurs.changeChevaletEtat(NumJoueur,k,0);
+					
+				}
+			}
+			for(int i=0;i<15;i++) {
+				for(int j=0;j<15;j++) {
+					if(source == vue.grilleButton[i][j]) {
+						if(lettreselect!=null) {
+							model.addLettrePlateau(i,j,lettreselect);
+							lettreselect = null;
+						}
+						
+					}
+				}
+			}
+		}
+		if(clic == MouseEvent.BUTTON3){
+			for(int i=0;i<15;i++) {
+				for(int j=0;j<15;j++) {
+					if(source == vue.grilleButton[i][j]) {
+						lettreselect = model.getMatriceLettre(j,i);
+						if(lettreselect!=null) {
+							model.removeLettrePlateau(i,j);
+							listeJoueurs.addLettreChevalet(NumJoueur,lettreselect);
+						}
+						
+					}
+				}
+			}
+		}
+		
+		
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
