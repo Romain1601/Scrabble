e3dc81bcce753619df870505d1a70140488b0483
loic-dev <loic.charrie.12@gmail.com>
2020-03-26 20:13:40 +0100

    update modele joueur

--- src/com/sdz/model/Joueur.java
+++ src/com/sdz/model/Joueur.java
@@ -5,18 +5,21 @@ public class Joueur {
 	String name;
 	int numero;
 	Model_Chevalet chevalet;
+	Model_Pioche pioche;
 	int point;
 	
-	public Joueur(String nom, int num, int p) {
+	public Joueur(String nom, int num, int p, Model_Pioche pi) {
+		this.pioche = pi;
 		this.name =nom;
 		this.numero =num;
-		chevalet = new Model_Chevalet();
+		chevalet = new Model_Chevalet(pioche);
 		this.point = p;
 	}
 	
 	public String getNameJoueur() {
 		return name;
 	}
+	
 	
 	public int getPointJoueur() {
 		return point;
