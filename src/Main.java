e3c7259aff8bb5c67594db2ec707ac09e70b4eb8
loic-dev <loic.charrie.12@gmail.com>
2020-03-31 13:47:56 +0200

    update du main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 
+import com.sdz.model.Model_Pioche;
 import com.sdz.vue.Vue;
 
 
 public class Main {
 	
+	static Model_Pioche modelPioche = new Model_Pioche(); 
+
+	
 	public static void main(String[] args) {
-		Vue vue = new Vue();
+		Vue vue = new Vue(modelPioche);
 		vue.getVue();
 
 	}
