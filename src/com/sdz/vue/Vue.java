1e06cff8a3889d1ace0025809defb844dfb05cf2
loic-dev <loic.charrie.12@gmail.com>
2020-03-31 13:46:23 +0200

    udpate de la fenetre

--- src/com/sdz/vue/Vue.java
+++ src/com/sdz/vue/Vue.java
@@ -2,13 +2,10 @@ package com.sdz.vue;
 
 
 
-import java.awt.Color;
-import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 import com.sdz.model.ListeJoueurs;
 import com.sdz.model.Model_Choix_Joueur;
@@ -25,9 +22,9 @@ public class Vue extends JFrame   {
 	
 
 	
-	Model_Pioche modelPioche = new Model_Pioche(); 
-	Vue_Pioche vue_pioche = new Vue_Pioche(modelPioche);
-	Vue_Choix_Joueur menuJoueur = new Vue_Choix_Joueur(modelPioche,this);
+	Model_Pioche modelPioche;
+	Vue_Pioche vue_pioche;
+	Vue_Choix_Joueur menuJoueur;
 	
 	
 
@@ -38,34 +35,61 @@ public class Vue extends JFrame   {
 	JFrame fenetre = new JFrame(); 
 	JPanel inside = new JPanel(); 
 	
-	public Vue() {
+	public Vue(Model_Pioche m) {
+		this.modelPioche =m;
+		vue_pioche = new Vue_Pioche(modelPioche);
+		menuJoueur = new Vue_Choix_Joueur(modelPioche,this);
 		fenetre.setTitle("Scrabble");
 		fenetre.setSize(1000, 1000);
 		fenetre.setLocationRelativeTo(null); 
         fenetre.setResizable(false);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenetre.setLayout(null);
-        fenetre.setVisible(true);
         inside.add(menuJoueur.Getformulaire());
         inside.setBounds(0,0,1000,1000);
         inside.setLayout(null);
-        fenetre.add(inside);
+        fenetre.setContentPane(inside);
+        fenetre.setVisible(true);
+        
        
     }
 	
 	
-	public void start(Model_Choix_Joueur j) {
+	public void start(Model_Choix_Joueur j) { 
 		inside.removeAll();
 		listeJoueurs = new ListeJoueurs(modelPioche , j.getlisteJoueur());
 		Vue_chevalet_joueur = new Vue_Chevalet_Joueur(j.getlisteJoueur().get(0).getNumJoueur(), modelPioche, listeJoueurs);
-		vue_plateau = new Vue_Plateau(listeJoueurs, j.getlisteJoueur().get(0).getNumJoueur());
+		vue_plateau = new Vue_Plateau(listeJoueurs, j.getlisteJoueur().get(0).getNumJoueur(),this,j);
 		vue_point = new Vue_Point_Joueur(listeJoueurs);
 		inside.add(vue_plateau.GetVuePlateau());
+		inside.add(vue_plateau.GetVuevalider());
+		
 		inside.add(vue_pioche.GetVuePioche());
 		inside.add(Vue_chevalet_joueur.GetVueNomJoueur());
 		inside.add(Vue_chevalet_joueur.GetVueChevaletJoueur());
 		inside.add(Vue_chevalet_joueur.GetVueChevaletBoutton());
 		inside.add(vue_point.getVuePoint());
+		inside.updateUI();
+	}
+	
+	
+	public void changeJoueur(Model_Choix_Joueur j, int i) { 
+		inside.remove(Vue_chevalet_joueur.GetVueNomJoueur());
+		inside.remove(Vue_chevalet_joueur.GetVueChevaletJoueur());
+		inside.remove(Vue_chevalet_joueur.GetVueChevaletBoutton());
+		inside.remove(vue_plateau.GetVuePlateau());
+		inside.remove(vue_plateau.GetVuevalider());
+		//listeJoueurs = new ListeJoueurs(modelPioche , j.getlisteJoueur());
+		Vue_chevalet_joueur = new Vue_Chevalet_Joueur(j.getlisteJoueur().get(i).getNumJoueur(), modelPioche, listeJoueurs);
+		vue_plateau.changeControleur(i);
+		vue_plateau.update("valider");
+		
+		
+		inside.add(vue_plateau.GetVuePlateau());
+		inside.add(vue_plateau.GetVuevalider());
+		inside.add(Vue_chevalet_joueur.GetVueNomJoueur());
+		inside.add(Vue_chevalet_joueur.GetVueChevaletJoueur());
+		inside.add(Vue_chevalet_joueur.GetVueChevaletBoutton());
 		inside.updateUI();
 	}
 	
